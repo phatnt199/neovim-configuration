ad3c1693ea9716ff4da4d211119a7b606f7c2ee8
Phat Nguyen <tanphat199@gmail.com>
2022-07-24 12:54:05 +0900

    update lsp

--- lua/settings/300-lsp.lua
+++ lua/settings/300-lsp.lua
@@ -28,33 +28,18 @@ cmp.setup({
 
 local cmpLsp = require'cmp_nvim_lsp'
 local capabilities = cmpLsp.update_capabilities(vim.lsp.protocol.make_client_capabilities())
-
-require('lspconfig').dartls.setup {
-  capabilities = capabilities,
-  flags = {
-    debounce_text_changes = 150,
-  }
+local flags = { debounce_text_changes = 150 }
+local handlers = {
+  ["textDocument/publishDiagnostics"] = vim.lsp.with(
+  vim.lsp.diagnostic.on_publish_diagnostics, {
+    virtual_text = false,
+    underline = true,
+    update_in_insert = false,
+  }),
 }
 
-require('lspconfig').tsserver.setup {
-  capabilities = capabilities,
-  flags = {
-    debounce_text_changes = 150,
-  },
-  handlers = {
-    ["textDocument/publishDiagnostics"] = vim.lsp.with(
-    vim.lsp.diagnostic.on_publish_diagnostics, {
-      virtual_text = false,
-      underline = true,
-      update_in_insert = false,
-    }
-    ),
-  }
-}
+local defaultConfigs =
 
-require('lspconfig').rust_analyzer.setup {
-  capabilities = capabilities,
-  flags = {
-    debounce_text_changes = 150,
-  }
-}
+require('lspconfig').dartls.setup { capabilities = capabilities, flags = flags, handlers = handlers }
+require('lspconfig').tsserver.setup { capabilities = capabilities, flags = flags, handlers = handlers }
+require('lspconfig').rust_analyzer.setup { capabilities = capabilities, flags = flags, handlers = handlers }

1081a7f0e9b526fd1abccba8a03699cfe499ee68
Phat Nguyen <tanphat199@gmail.com>
2023-04-13 22:16:42 +0700

    fix

--- lua/settings/003-config.lua
+++ lua/settings/003-config.lua
@@ -51,7 +51,6 @@ require('toggleterm').setup({
 require('nvim-tree').setup({
   disable_netrw = true,
   hijack_netrw = true,
-  open_on_setup = false,
   open_on_tab = false,
   update_cwd = true,
   update_focused_file = {

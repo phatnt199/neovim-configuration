279a6c9e7d32f31c4f75796256cf7dbea233394d
Phat Nguyen <tanphat199@gmail.com>
2023-10-03 13:06:05 +0700

    remove duplicated vim keymap

--- lua/main/003-keymaps.lua
+++ lua/main/003-keymaps.lua
@@ -1,60 +1,67 @@
-vim.keymap.set('n', '<C-s>',        ':w<CR>')
-vim.keymap.set('n', '<M-k>',        '<C-y>')
-vim.keymap.set('n', '<M-j>',        '<C-e>')
-vim.keymap.set('n', '<M-h>',        'zh')
-vim.keymap.set('n', '<M-l>',        'zl')
-vim.keymap.set('n', '<C-z>',        ':undo<CR>')
-vim.keymap.set('n', '<C-y>',        ':redo<CR>')
-vim.keymap.set('n', '<C-Up>',       ':resize -2<CR>')
-vim.keymap.set('n', '<C-Down>',     ':resize +2<CR>')
-vim.keymap.set('n', '<leader>rf',   ':luafile %<CR>')
+local keymaps = {
+  { mode = 'n', shortcut = '<C-s>',        fn = ':w<CR>' },
+  { mode = 'n', shortcut = '<M-k>',        fn = '<C-y>'},
+  { mode = 'n', shortcut = '<M-j>',        fn = '<C-e>'},
+  { mode = 'n', shortcut = '<M-h>',        fn = 'zh'},
+  { mode = 'n', shortcut = '<M-l>',        fn = 'zl'},
+  { mode = 'n', shortcut = '<C-z>',        fn = ':undo<CR>'},
+  { mode = 'n', shortcut = '<C-y>',        fn = ':redo<CR>'},
+  { mode = 'n', shortcut = '<C-Up>',       fn = ':resize -2<CR>'},
+  { mode = 'n', shortcut = '<C-Down>',     fn = ':resize +2<CR>'},
+  { mode = 'n', shortcut = '<leader>rf',   fn = ':luafile %<CR>'},
 
-vim.keymap.set('n', '<leader>ts',   ':Telescope<CR>')
-vim.keymap.set('n', '<C-f>',        ':Telescope current_buffer_fuzzy_find<CR>')
-vim.keymap.set('n', '<C-p>',        ':Telescope git_files<CR>')
-vim.keymap.set('n', '<C-r>',        ':Telescope find_files<CR>')
-vim.keymap.set('n', '<leader>ff',   ':Telescope live_grep<CR>')
-vim.keymap.set('n', '<leader>fs',   ':Telescope grep_string<CR>')
-vim.keymap.set('n', '<leader>gc',   ':Telescope git_commits<CR>')
-vim.keymap.set('n', '<leader>gst',  ':Telescope git_status<CR>')
-vim.keymap.set('n', '<leader>gb',   ':Telescope git_branches<CR>')
+  { mode = 'n', shortcut = '<leader>ts',   fn = ':Telescope<CR>'},
+  { mode = 'n', shortcut = '<C-f>',        fn = ':Telescope current_buffer_fuzzy_find<CR>'},
+  { mode = 'n', shortcut = '<C-p>',        fn = ':Telescope git_files<CR>'},
+  { mode = 'n', shortcut = '<C-r>',        fn = ':Telescope find_files<CR>'},
+  { mode = 'n', shortcut = '<leader>ff',   fn = ':Telescope live_grep<CR>'},
+  { mode = 'n', shortcut = '<leader>fs',   fn = ':Telescope grep_string<CR>'},
+  { mode = 'n', shortcut = '<leader>gc',   fn = ':Telescope git_commits<CR>'},
+  { mode = 'n', shortcut = '<leader>gst',  fn = ':Telescope git_status<CR>'},
+  { mode = 'n', shortcut = '<leader>gb',   fn = ':Telescope git_branches<CR>'},
 
-vim.keymap.set('n', 'nv',           ':NvimTreeToggle<CR>')
-vim.keymap.set('n', 'nr',           ':NvimTreeRefresh<CR>')
-vim.keymap.set('n', 'nf',           ':NvimTreeFocus<CR>')
+  { mode = 'n', shortcut = 'nv',           fn = ':NvimTreeToggle<CR>'},
+  { mode = 'n', shortcut = 'nr',           fn = ':NvimTreeRefresh<CR>'},
+  { mode = 'n', shortcut = 'nf',           fn = ':NvimTreeFocus<CR>'},
 
-vim.keymap.set('n', '<space>e',     ':lua vim.diagnostic.open_float()<CR>')
-vim.keymap.set('n', ']d',           ':lua vim.diagnostic.goto_next()<CR>')
-vim.keymap.set('n', '[d',           ':lua vim.diagnostic.goto_prev()<CR>')
-vim.keymap.set('n', '<space>q',     ':lua vim.diagnostic.setloclist()<CR>')
-vim.keymap.set('n', 'gD',           ':lua vim.lsp.buf.declaration()<CR>')
-vim.keymap.set('n', 'gd',           ':lua vim.lsp.buf.definition()<CR>')
-vim.keymap.set('n', 'K',            ':lua vim.lsp.buf.hover()<CR>')
-vim.keymap.set('n', 'gi',           ':lua vim.lsp.buf.implementation()<CR>')
-vim.keymap.set('n', '<C-k>',        ':lua vim.lsp.buf.signature_help()<CR>')
-vim.keymap.set('n', '<leader>D',    ':lua vim.lsp.buf.type_definition()<CR>')
-vim.keymap.set('n', '<leader>rn',   ':lua vim.lsp.buf.rename()<CR>')
-vim.keymap.set('n', '<leader>ac',   ':lua vim.lsp.buf.code_action()<CR>')
-vim.keymap.set('n', '<leader>oi',   ':lua vim.lsp.buf.code_action({ context = { only = { "source.organizeImports" } }, apply = true })<CR>')
-vim.keymap.set('n', 'gr',           ':lua vim.lsp.buf.references()<CR>')
-vim.keymap.set('n', 'ff',           ':FormatWrite<CR>')
+  { mode = 'n', shortcut = '<space>e',     fn = ':lua vim.diagnostic.open_float()<CR>'},
+  { mode = 'n', shortcut = ']d',           fn = ':lua vim.diagnostic.goto_next()<CR>'},
+  { mode = 'n', shortcut = '[d',           fn = ':lua vim.diagnostic.goto_prev()<CR>'},
+  { mode = 'n', shortcut = '<space>q',     fn = ':lua vim.diagnostic.setloclist()<CR>'},
+  { mode = 'n', shortcut = 'gD',           fn = ':lua vim.lsp.buf.declaration()<CR>'},
+  { mode = 'n', shortcut = 'gd',           fn = ':lua vim.lsp.buf.definition()<CR>'},
+  { mode = 'n', shortcut = 'K',            fn = ':lua vim.lsp.buf.hover()<CR>'},
+  { mode = 'n', shortcut = 'gi',           fn = ':lua vim.lsp.buf.implementation()<CR>'},
+  { mode = 'n', shortcut = '<C-k>',        fn = ':lua vim.lsp.buf.signature_help()<CR>'},
+  { mode = 'n', shortcut = '<leader>D',    fn = ':lua vim.lsp.buf.type_definition()<CR>'},
+  { mode = 'n', shortcut = '<leader>rn',   fn = ':lua vim.lsp.buf.rename()<CR>'},
+  { mode = 'n', shortcut = '<leader>ac',   fn = ':lua vim.lsp.buf.code_action()<CR>'},
+  { mode = 'n', shortcut = '<leader>oi',   fn = ':lua vim.lsp.buf.code_action({ context = { only = { "source.organizeImports" } }, apply = true })<CR>'},
+  { mode = 'n', shortcut = 'gr',           fn = ':lua vim.lsp.buf.references()<CR>'},
+  { mode = 'n', shortcut = 'ff',           fn = ':FormatWrite<CR>'},
 
-vim.keymap.set('n', '<C-t>',        ':tabnew<CR>')
-vim.keymap.set('n', '<C-x>',        ':tabclose<CR>')
+  { mode = 'n', shortcut = '<C-t>',        fn = ':tabnew<CR>'},
+  { mode = 'n', shortcut = '<C-x>',        fn = ':tabclose<CR>'},
 
-vim.keymap.set('i', 'jk',           '<Esc>')
+  { mode = 'i', shortcut = 'jk',           fn = '<Esc>'},
 
-vim.keymap.set('v', 'J',            ':m \'>+1<CR>gv=gv')
-vim.keymap.set('v', 'K',            ':m \'<-2<CR>gv=gv')
+  { mode = 'v', shortcut = 'J',            fn = ':m \'>+1<CR>gv=gv'},
+  { mode = 'v', shortcut = 'K',            fn = ':m \'<-2<CR>gv=gv'},
 
-vim.keymap.set('x', '<leader>p',    '\"_dP')
-vim.keymap.set('n', '<leader>y',    '\"+y')
-vim.keymap.set('v', '<leader>y',    '\"+y')
-vim.keymap.set('n', '<leader>Y',    '\"+Y')
+  { mode = 'x', shortcut = '<leader>p',    fn = '\"_dP'},
+  { mode = 'n', shortcut = '<leader>y',    fn = '\"+y'},
+  { mode = 'v', shortcut = '<leader>y',    fn = '\"+y'},
+  { mode = 'n', shortcut = '<leader>Y',    fn = '\"+Y'},
 
-vim.keymap.set('n', '<leader>gl',    ':diffget LOCAL')
-vim.keymap.set('n', '<leader>gb',    ':diffget BASE')
-vim.keymap.set('n', '<leader>gr',    ':diffget REMOTE')
+  { mode = 'n', shortcut = '<leader>gl',   fn = ':diffget LOCAL'},
+  { mode = 'n', shortcut = '<leader>gb',   fn = ':diffget BASE'},
+  { mode = 'n', shortcut = '<leader>gr',   fn = ':diffget REMOTE'},
+}
+
+for index in ipairs(keymaps) do
+  local keymap = keymaps[index]
+  vim.keymap.set(keymap.mode, keymap.shortcut, keymap.fn)
+end
 
 vim.cmd [[
   sign define DiagnosticSignError text=✖  texthl=DiagnosticSignError  linehl=   numhl=DiagnosticLineNrError
